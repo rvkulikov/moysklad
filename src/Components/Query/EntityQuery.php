b8a3932c6d42d1d70bb0a633caa5fd84923cf29d
Roman Kulikov <r.v.kulikov@yandex.ru>
2020-01-30 02:46:30 +0300

    fix by id query

--- src/Components/Query/EntityQuery.php
+++ src/Components/Query/EntityQuery.php
@@ -6,41 +6,77 @@ use MoySklad\Components\Expand;
 use MoySklad\Entities\AbstractEntity;
 use MoySklad\Lists\EntityList;
 use MoySklad\Registers\ApiUrlRegistry;
+use Throwable;
 
-class EntityQuery extends AbstractQuery {
+/**
+ *
+ */
+class EntityQuery extends AbstractQuery
+{
     protected static $entityListClass = EntityList::class;
 
     /**
      * Get entity by id
-     * @param $id
+     *
+     * @param             $id
      * @param Expand|null $expand Deprecated, use withExpand()
+     *
      * @return AbstractEntity
-     * @throws \Throwable
+     * @throws Throwable
      */
-    public function byId($id, Expand $expand = null){
-        if ( !$expand ) $expand = $this->expand;
+    public function byId($id, Expand $expand = null)
+    {
+        if ($expand === null) {
+            $expand = $this->expand;
+        }
+
+        $data = array_filter([
+            $expand ? $expand->flatten() : [],
+            $this->querySpecs->toArray()
+        ]);
+
+        $data = !empty($data)
+            ? array_merge(...$data)
+            : [];
+
         $res = $this->getSkladInstance()->getClient()->get(
             ApiUrlRegistry::instance()->getByIdUrl($this->entityName, $id),
-            ($expand?['expand'=>$expand->flatten()]:[]),
+            $data,
             $this->requestOptions
         );
         return new $this->entityClass($this->getSkladInstance(), $res);
     }
 
-	/**
-	 * Get entity by syncid
-	 * @param $id
-	 * @param Expand|null $expand Deprecated, use withExpand()
-	 * @return AbstractEntity
-	 * @throws \Throwable
-	 */
-	public function bySyncId($id, Expand $expand = null){
-		if ( !$expand ) $expand = $this->expand;
-		$res = $this->getSkladInstance()->getClient()->get(
-			ApiUrlRegistry::instance()->getBySyncIdUrl($this->entityName, $id),
-			($expand?['expand'=>$expand->flatten()]:[]),
-			$this->requestOptions
-		);
-		return new $this->entityClass($this->getSkladInstance(), $res);
-	}
+    /**
+     * Get entity by syncid
+     *
+     * @param             $id
+     * @param Expand|null $expand Deprecated, use withExpand()
+     *
+     * @return AbstractEntity
+     * @throws Throwable
+     */
+    public function bySyncId($id, Expand $expand = null)
+    {
+        if ($expand === null) {
+            $expand = $this->expand;
+        }
+
+        $data = array_filter([
+            $expand ? $expand->flatten() : [],
+            $this->querySpecs->toArray()
+        ]);
+
+        $data = !empty($data)
+            ? array_merge(...$data)
+            : [];
+
+
+        $res = $this->getSkladInstance()->getClient()->get(
+            ApiUrlRegistry::instance()->getBySyncIdUrl($this->entityName, $id),
+            $data,
+            $this->requestOptions
+        );
+        return new $this->entityClass($this->getSkladInstance(), $res);
+    }
 }
